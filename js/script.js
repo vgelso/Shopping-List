de78f8eea81595829cbc1dffbe6622d5097bae02
vgelso <gelsojobs@gmail.com>
2015-07-26 01:43:27 -0700

    jQuery for row manipulation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var rowNum = 0; // roq number
+
 $(document).ready(function() {
 
 	$(document).keypress(function(event){
@@ -20,17 +22,43 @@ $(document).ready(function() {
 	        	alert('Please enter an Item');
 	    	}
 	    	else {
-	    		var html = "<p class=\"added\"><img class=\"delete-img\" src=\"images/delete-image.png\" alt=\"Delete image\"/>" +
-	    				   str + "<img class=\"checked-img\" src=\"images/checked-image.png\" alt=\"Checked image\"/></p>"
-	    		$( "#add-item" ).after(html);
+	    		var html = "<p class=\"added\" id=\"row_" + rowNum + "\"><img class=\"delete-img\" id=\"delete-img_" + rowNum + "\" src=\"images/delete-image2.png\" alt=\"Delete image\"/>" +
+	    				   "<span id=\"word_" + rowNum + "\">" + str + "</span> <img class=\"checked-img\" src=\"images/checked-image2.png\" id=\"checked-img_" + rowNum + "\" alt=\"Checked image\"/></p>"
+	    		$(".slider-container").append(html);
 	    		$("#add-item").val(""); // clear input after item is added
+	    		checkRowCount();
+
+	    		rowNum++; // add one (increment) to current row number rowNum = rowNum + 1
 	    	}
 	    }
 	});
 
-	$("#add-item").on("click", function(e) {
-		alert(e);
+	$("body").on("click", ".delete-img", function(e) {
+		var parent = getParent(e);
+		alert(parent);
+		$(parent).fadeOut("fast", function() { $(this).remove() });
+	});
+
+	$("body").on("click", ".checked-img", function(e) {
+		var previousElement = "#" + $("#" + e.target.id).prev().attr("id");
+		var parent = getParent(e);
+		alert(parent);
+		$(previousElement).wrap("<strike>");
+		$(parent).animate( { backgroundColor: "#99CC99" }, "slow");
+		//$(parent).css("background-color", "#006633");	
+		$(parent).css("color", "#3D5C5C");	
 	});
 
 });
-	
+
+function getParent(e) {
+	return "#" + $("#" + e.target.id).parent().attr("id");
+}
+
+function checkRowCount() {
+	// get current number of rows
+	var count = $(".slider-container").children().length;
+	if (count > 5) {
+		// slide content to left
+	}
+}
